6163dbd316300f74a981fc36ad1ca43d82bf9276
Wasim Raja Khan <wasimraja.k@valuelabs.com>
2025-01-07 11:42:26 +0530

    task-1

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,14 +1,50 @@
-import { List } from 'ui'
+import { useState, useEffect } from "react";
 
-const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
+export default function PokemonList() {
+  // Defining constants that are used
+  const [pokemon, setPokemon] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-const App = () => {
+  // Using useEffect hook for fetching the data from the api.
+  useEffect(() => {
+    const fetchPokemons = async () => {
+      // Using try catch blocks to set up the things and handle errors.
+      try {
+        const response = await fetch(
+          "https://pokeapi.co/api/v2/pokemon?limit=151"
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch Pokemon data");
+        }
+        const data = await response.json();
+        setPokemon(data.results); // update the state with pokemon list
+        setLoading(false); // turn off loading
+      } catch (error) {
+        setError(error); // Handle error
+        setLoading(false);
+      }
+    };
+    fetchPokemons();
+  }, []); // Empty dependency array to fetch only once
+
+  // Loading UI while the in the first setup.
+  if (loading) return <div>Loading...</div>;
+
+  // Displaying error messages to define the errors
+  // that helps to understand the flow.
+  if (error) <div>Error: {error.message}</div>;
+
   return (
-  <>
-    <h1>Pokemon list:</h1>
-    <List />
-  </>
-  )
+    <div>
+      <h1>Pokémon List</h1>
+      <ul>
+        {pokemon.map((pokemon, index) => (
+          <li key={index}>
+            <span>{pokemon.name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-export default App
---
+++ project/apps/app/src/task1.md
@@ -0,0 +1,19 @@
+# Code Challenge: Task1 Questions - 
+
+## Question no 1:
+How did you manage to fetch the list and what tool did you use?
+
+## Answer no 1: 
+I have used the fetch API to make a GET request to the Pokémon API and store the response in the component's state. I'll also use the useState and useEffect hooks from React to manage the state and make the API call.
+
+## Question no 2:
+What steps would you take to future improve this?
+
+## Answer no 2:
+I can be improving it by - 
+1. Caching
+2. Debouncing
+3. Pagination
+4. Memoization
+5. Optimizing API Request
+6. Using Axios library to speed up the fetching process etc
